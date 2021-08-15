b2f029d50156ab907f5d46d7d5e22b60b2e97364
ctuck99 <chrisgtucker@hotmail.com>
2021-08-14 18:27:42 -0700

    Update script.js
    Added remaining steps of Skills Network Labs

--- script.js
+++ script.js
@@ -43,4 +43,51 @@ Once the user clicks on the alert "OK" button, take the user back to the "Princi
 You are done with the coding. Let us proceed to next exercise where we test the code.
 
 Write comments in your code. They not only help you score more marks in the project, but also help you to debug and maintain the code in the long term.
+
+PREVIOUS PART WAS EXERCISE 4
+NEXT IS 5
+
+Exercise 5 : Test the app.
+Now that you have finished coding your app. Let us do some basic testing, before we release it.
+
+Enter these values in the form.
+
+Amount = 0
+
+Rate = 1
+
+No. of Years = 1
+
+Click on Compute button.
+
+You should see an alert "Enter a positive number".
+
+Enter these values in the form.
+
+Amount = 99999
+
+Rate = 9
+
+No. of Years = 9
+
+Click on Compute button. You should see the following output.
+If you deposit 99999,<br>
+at an interest rate of 9.<br>
+You will receive an amount of 80999.19,<br>
+in the year 2029<br>
+
+As I write this, the year is 2020, so 2029 is correct.
+
+If you app has passed both the test cases, it is time to go to the next excercise.
+
+Otherwise, start debugging your code to see where you went wrong.
+
+Exercise 6 : Upload to GitHub and submit the github pages URL for peer review.
+When you are done with all the changes in the project, commit and push to GitHub. You do not have to create a Push Request back to the original IBM repository.
+
+Open the URL of your project in a browser. (You created this URL in Exercise 1)
+
+You should see your completed project.
+
+Submit this URL for peer review.
 */
