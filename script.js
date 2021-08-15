09683e46fd2a9a03162cf8eab3efe11ae1e9949c
ctuck99 <chrisgtucker@hotmail.com>
2021-08-14 18:25:29 -0700

    Update script.js
    Added remaining steps form https://labs.cognitiveclass.ai/tools/theiadocker/?md_instructions_url=https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-CD0101EN-SkillsNetwork/labs/Project/project-instructions.md&lti=true

--- script.js
+++ script.js
@@ -13,3 +13,34 @@ function updateRate()
 	document.getElementById("rate_val").innerText = rateval;
 }
         
+/* Remaing steps
+Link this function with an "onchange" event on the range input.
+
+Change the slider, and test if the display in the span is being updated dynamically.
+
+Get the reference to the element named 'result'
+
+When "Compute Interest" is clicked, set its inner html property to the below text.
+
+If you deposit 1000000,
+at an interest rate of 3.5%.
+You will receive an amount of 175000,
+in the year 2025
+
+The number above are for indication only. Make sure the output contains the relevant values by using the correct variables
+
+Sample:
+document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
+
+Make sure that the input you have taken as "No of Years" is converted into an actual year.
+
+Make sure the numbers in the result are highlighted.
+
+Add validation for "Principal" input box. If the user enters zero or negative values, show an alert "Enter a positive number"
+
+Once the user clicks on the alert "OK" button, take the user back to the "Principal" input box, by setting the focus on this box. You can refer to the Javascript Form Validation lab.
+
+You are done with the coding. Let us proceed to next exercise where we test the code.
+
+Write comments in your code. They not only help you score more marks in the project, but also help you to debug and maintain the code in the long term.
+*/
